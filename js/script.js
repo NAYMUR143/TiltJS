d674d4fee5c6c86a6739e70b858d496c4fabb6e1
NAYMUR143 <66328660+NAYMUR143@users.noreply.github.com>
2021-11-21 21:05:51 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelector(".card"), {
+  max: 20,
+  speed: 600,
+  scale: 1.1,
+  transition: true,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  perspective: 400,
+  glare: true,
+});
